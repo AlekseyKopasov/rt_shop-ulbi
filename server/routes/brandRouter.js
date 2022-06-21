fb222b67a717a716ccfeecad2667241fda6241e3
Aleksey Kopasov <alesey.k1@gmail.com>
2022-06-22 01:13:58 +0300

    WIP: создает CRUD для типов, брэндов, устройств

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,8 @@
 const Router = require('express')
 const router = new Router
-const BrandController = require('../controllers/brandController')
+const brandController = require('../controllers/brandController')
 
-router.post('/', BrandController.create)
-router.get('/', BrandController.getAll)
+router.post('/', brandController.create)
+router.get('/', brandController.getAll)
 
 module.exports = router
