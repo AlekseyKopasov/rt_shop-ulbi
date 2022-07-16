703338b4f31548a603809566782233d716d2fd13
Aleksey Kopasov <alesey.k1@gmail.com>
2022-07-17 00:02:27 +0300

    добаляет слушателя на карточку товара

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Card, Col, Image} from 'react-bootstrap'
 import star from '../assets/star.svg'
+import {useNavigate} from 'react-router-dom'
+import {DEVICE_ROUTE} from '../utils/consts'
 
 const DeviceItem = ({device}) => {
+  const navigate = useNavigate()
+
   return (
-    <Col md={3}>
-      <Card className="mt-3 w-auto" style={{width: 150, cursor: 'pointer', border: 'light', padding: 5}}>
+    <Col md={3} className="mt-3 w-auto" onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
+      <Card style={{width: 150, cursor: 'pointer', border: 'light', padding: 5}}>
         <Image src={device.img}/>
         <div className="text-black-50 d-flex justify-content-between">
           <div>Title....</div>
