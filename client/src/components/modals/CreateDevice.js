7c0084d2f72aaa16f1f240ee45a55f9c53ecf381
Aleksey Kopasov <alesey.k1@gmail.com>
2022-07-18 23:46:30 +0300

    добавляет устройства и характеристики

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -4,8 +4,11 @@ import {Context} from '../../index'
 import DropdownToggle from 'react-bootstrap/DropdownToggle'
 import DropdownItem from 'react-bootstrap/DropdownItem'
 import DropdownMenu from 'react-bootstrap/DropdownMenu'
+import {useEffect} from 'react'
+import {createDevice, fetchBrands, fetchDevices, fetchTypes} from '../../http/deviceAPI'
+import {observer} from 'mobx-react-lite'
 
-const CreateDevice = ({show, onHide}) => {
+const CreateDevice = observer(({show, onHide}) => {
   const {devices} = useContext(Context)
   const [info, setInfo] = useState([])
   const [name, setName] = useState('')
@@ -13,6 +16,11 @@ const CreateDevice = ({show, onHide}) => {
   const [type, setType] = useState(null)
   const [brand, setBrand] = useState(null)
   const [image, setImage] = useState(null)
+
+  useEffect(() => {
+    fetchTypes().then(data => devices.setTypes(data))
+    fetchBrands().then(data => devices.setBrands(data))
+  }, [])
 
   const addInfo = () => {
     setInfo([...info, {title: '', description: '', number: Date.now()}])
@@ -24,6 +32,22 @@ const CreateDevice = ({show, onHide}) => {
 
   const selectFile = e => {
     setImage(e.target.files[0])
+  }
+
+  const changeInfo = (key, number, value) => {
+    setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+  }
+
+  const addDevice = () => {
+    const formData = new FormData()
+    formData.append('name', name)
+    formData.append('price', `${price}`)
+    formData.append('img', image)
+    formData.append('brandId', devices.selectedBrand.id)
+    formData.append('typeId', devices.selectedType.id)
+    formData.append('info', JSON.stringify(info))
+
+    createDevice(formData).then(data => onHide())
   }
 
   return (
@@ -43,20 +67,40 @@ const CreateDevice = ({show, onHide}) => {
         <Form className="d-flex">
           <Row className="d-flex flex-column me-3">
             <Dropdown className="mb-2">
-              <DropdownToggle variant={'outline-dark'} className="w-100">Выберите тип</DropdownToggle>
-              <DropdownMenu>
+              <Dropdown.Toggle
+                variant={'outline-dark'}
+                className="w-100"
+              >
+                {devices.selectedType.name || 'Выберите тип'}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
                 {devices.types.map(type =>
-                  <DropdownItem key={type.id}>{type.name}</DropdownItem>
+                  <Dropdown.Item
+                    onClick={() => devices.setSelectedType(type)}
+                    key={type.id}
+                  >
+                    {type.name}
+                  </Dropdown.Item>
                 )}
-              </DropdownMenu>
+              </Dropdown.Menu>
             </Dropdown>
             <Dropdown className="mb-2">
-              <DropdownToggle variant={'outline-dark'} className="w-100">Выберите бренд</DropdownToggle>
-              <DropdownMenu>
+              <Dropdown.Toggle
+                variant={'outline-dark'}
+                className="w-100"
+              >
+                {devices.selectedBrand.name || 'Выберите бренд'}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
                 {devices.brands.map(brand =>
-                  <DropdownItem key={brand.id}>{brand.name}</DropdownItem>
+                  <Dropdown.Item
+                    onClick={() => devices.setSelectedBrand(brand)}
+                    key={brand.id}
+                  >
+                    {brand.name}
+                  </Dropdown.Item>
                 )}
-              </DropdownMenu>
+              </Dropdown.Menu>
             </Dropdown>
           </Row>
           <Row>
@@ -95,7 +139,9 @@ const CreateDevice = ({show, onHide}) => {
                   className="ps-0 w-auto"
                 >
                   <Form.Control
+                    value={i.title}
                     placeholder={'Название характеристи'}
+                    onChange={(e) => setInfo('title', e.target.value, i.number)}
                   />
                 </Col>
                 <Col
@@ -103,7 +149,9 @@ const CreateDevice = ({show, onHide}) => {
                   className="ps-0 w-auto"
                 >
                   <Form.Control
+                    value={i.description}
                     placeholder={'Описание характеристи'}
+                    onChange={(e) => setInfo('description', e.target.value, i.number)}
                   />
                 </Col>
                 <Col
@@ -125,10 +173,10 @@ const CreateDevice = ({show, onHide}) => {
       </Modal.Body>
       <Modal.Footer>
         <Button variant={"outline-danger"} onClick={onHide}>Закрыть</Button>
-        <Button variant={"outline-success"} onClick={onHide}>Добавить</Button>
+        <Button variant={"outline-success"} onClick={addDevice}>Добавить</Button>
       </Modal.Footer>
     </Modal>
   )
-}
+})
 
 export default CreateDevice
